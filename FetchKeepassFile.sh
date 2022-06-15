95ecbf7be83ec96df7cad525eb593708c3bf2db9
FH3095 <FH3095@users.noreply.github.com>
2022-06-16 01:27:34 +0200

    Make link configurable

--- FetchKeepassFile.sh
+++ FetchKeepassFile.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 
 . $SCRIPT_DIR/CloudData.sh
 
-OUTPUT=$(wget --user="$CLOUD_USER" --password="$CLOUD_PASS" --output-document="$SCRIPT_DIR/KeepassFile.kdbx" "https://cloud.4fh.eu/public.php/webdav/firefox.kdbx" 2>&1 )
+OUTPUT=$(wget --user="$CLOUD_USER" --password="$CLOUD_PASS" --output-document="$SCRIPT_DIR/KeepassFile.kdbx" "$CLOUD_LINK" 2>&1 )
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
         echo WGET ERROR $RESULT
