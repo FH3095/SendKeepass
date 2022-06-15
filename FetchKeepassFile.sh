14026399742bdafc667d260fec118bde6feeb1bf
FH3095 <FH3095@users.noreply.github.com>
2022-06-15 22:08:24 +0200

    Set and use SCRIPT_DIR

--- FetchKeepassFile.sh
+++ FetchKeepassFile.sh
@@ -1,8 +1,10 @@
 #/bin/sh
 
-. CloudData.sh
+SCRIPT_DIR=$(dirname $(realpath $0))
 
-OUTPUT=$(wget --user="$CLOUD_USER" --password="$CLOUD_PASS" --output-document="$TARGET_FILE" "https://cloud.4fh.eu/public.php/webdav/firefox.kdbx" 2>&1 )
+. $SCRIPT_DIR/CloudData.sh
+
+OUTPUT=$(wget --user="$CLOUD_USER" --password="$CLOUD_PASS" --output-document="$SCRIPT_DIR/KeepassFile.kdbx" "https://cloud.4fh.eu/public.php/webdav/firefox.kdbx" 2>&1 )
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
         echo WGET ERROR $RESULT
